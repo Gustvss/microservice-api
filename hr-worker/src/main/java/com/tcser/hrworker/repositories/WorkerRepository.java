cf74c53389417f39f65487f88878114427a02ee0
Gustavo Sant'Ana <gustvss.dev@gmail.com>
2023-06-05 09:23:47 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/tcser/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.tcser.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tcser.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+    
+}
