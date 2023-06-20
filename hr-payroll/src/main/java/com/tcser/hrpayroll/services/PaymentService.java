92877e5f665daa9107cbc43053ab82281d75464e
Gustavo Sant'Ana <gustvss.dev@gmail.com>
2023-06-20 16:49:43 -0300

    Update Springboot version

---
+++ hr-payroll/src/main/java/com/tcser/hrpayroll/services/PaymentService.java
@@ -0,0 +1,21 @@
+package com.tcser.hrpayroll.services;
+
+import com.tcser.hrpayroll.feignclients.WorkerFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tcser.hrpayroll.entities.Payment;
+import com.tcser.hrpayroll.entities.Worker;
+
+@Service
+public class PaymentService {
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
+	public Payment getPayment(long workerId, int days) {
+				
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+}
